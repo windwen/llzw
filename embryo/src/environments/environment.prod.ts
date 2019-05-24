5b565cca9cb7c6d41422e1ad0bec7048da3addb2
windwen <410499502@qq.com>
2019-05-24 20:42:07 +0800

    customer

---
+++ embryo/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC5kr77VEU-FnklR5DBn3bPwmtmpEjYde4",
+    authDomain: "embryo-version-2.firebaseapp.com",
+    databaseURL: "https://embryo-version-2.firebaseio.com",
+    projectId: "embryo-version-2",
+    storageBucket: "embryo-version-2.appspot.com",
+    messagingSenderId: "73552048992"
+  },
+  url1: 'http://localhost:8981/api/v1',
+};
